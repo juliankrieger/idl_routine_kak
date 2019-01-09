a4414b00c1db0abeeb3dbd122df65b14e1468b6e
juliankrieger <jkrieger@htwsaar.de>
2019-01-09 09:19:06 +0100

    added comments

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import struct
 import numpy as np
 
-ULONG_MASK = 0xffffffff
+ULONG_MASK = 0xffffffff # Use to manually unsign an integer
 
 def read_ulong(datastream):
-      date, = struct.unpack("<L", datastream.read(4))
-      return date
+    #Read Little Endian Long from datastream
+    date, = struct.unpack("<L", datastream.read(4))
+    return date
 
 def read_double_arr(size, datastream):
+    #Read little endian double from datastream into an array with size size
     data = np.fromfile(datastream, dtype=np.dtype('<d'), count=size)
+    #Then reshape it into Fortran index order
     array = np.reshape(data, size, order='F')
     return array
 
 def read_uint_arr(x,y,z, datastream):
+    #Read uint16 into a 1-D array with size x y z
     data = np.fromfile(datastream, dtype=np.uint16, count=x*y*z)
+    #then reshape it into a 3-D array with Fortran indexing order
     array = np.reshape(data, [x,y,z], order="F")
     return array
 
 if __name__ == "__main__":
+    # trivial
     with open("data.dat", "rb") as data:
 
         time_arr_size = read_ulong(data)
